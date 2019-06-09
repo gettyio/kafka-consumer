13e4581f7a071140d9a087461b195ebcae214939
Vinicius Niche Correa <viniciusnichecorrea@gmail.com>
2019-06-09 00:14:03 -0300

    updates consumer options

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
 import { Transform } from 'stream';
-import { ConsumerGroupStream } from 'kafka-node';
+import { ConsumerGroupStream, ConsumerGroupStreamOptions } from 'kafka-node';
 
 const {
-    KAFKA_HOST,
-    CONSUMER_GROUP_ID,
-    KAFKA_TOPIC
+  KAFKA_HOST,
+  CONSUMER_GROUP_ID,
+  KAFKA_TOPIC
 } = process.env;
 
-const consumerOptions = {
-    kafkaHost: KAFKA_HOST,
-    groupId: CONSUMER_GROUP_ID,
-    sessionTimeout: 15000,
-    asyncPush: false
-  };
-  
-  const consumerGroup = new ConsumerGroupStream(consumerOptions, KAFKA_TOPIC);
-  
-  const messageTransform = new Transform({
-    objectMode: true,
-    decodeStrings: true,
-    transform (message) {
-      console.log(`Received message ${message.value} transforming input`);
-    }
-  });
-  
-  consumerGroup.pipe(messageTransform);
+const consumerOptions: ConsumerGroupStreamOptions = {
+  kafkaHost: KAFKA_HOST,
+  groupId: CONSUMER_GROUP_ID,
+  autoCommit: false,
+  protocol: ['roundrobin'],
+  fromOffset: 'latest',
+  autoCommitIntervalMs: 1000,
+  fetchMaxBytes: 1024 * 1024,
+  fetchMaxWaitMs: 100,
+  encoding: 'utf8',
+  fetchMinBytes: 1
+};
+
+const consumerGroup = new ConsumerGroupStream(consumerOptions, KAFKA_TOPIC);
+
+const messageTransform = new Transform({
+  objectMode: true,
+  decodeStrings: true,
+  transform(message) {
+    console.log(`Received message ${message.value} transforming input`);
+  }
+});
+
+consumerGroup.pipe(messageTransform);
